cc3f2b2edc462bf4784e89e2cf0e6233a73629e9
ndiawar <dndiawar20@gmail.com>
2024-10-23 09:52:25 +0000

    MiniBank est une plateforme web de gestion de transactions financières

--- routes/web.php
+++ routes/web.php
@@ -55,3 +55,13 @@ Route::get('/clear-cache', function () {
     Artisan::call('route:clear');
     return "Cache is cleared";
 })->name('clear.cache');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
